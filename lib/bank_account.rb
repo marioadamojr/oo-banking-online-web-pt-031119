d40f37e9ca303faf47b3df6df15e1c0a55f698ba
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-13 19:41:56 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -15,4 +15,12 @@ class BankAccount
   def display_balance
     "Your balance is $#{@balance}."
   end
+
+  def valid?
+    if @status == "open" && @balance > 0
+      true
+    else
+      false
+
+  end
 end
